e71934c037d77507078e53af457dca68f64b7691
efao-fc <reinaldomavinga@gmail.com>
2025-07-02 01:57:41 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
+// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyD2RoJqL7RtYHNy8_s7Vs2WcpZHC5TNmSE",
   authDomain: "efao-fc-f4f36.firebaseapp.com",
   projectId: "efao-fc-f4f36",
-  storageBucket: "efao-fc-f4f36.firebasestorage.app",
+  storageBucket: "efao-fc-f4f36.appspot.com",
   messagingSenderId: "275309668658",
   appId: "1:275309668658:web:d62f98f8cb9c1168d34f2b"
 };
-
-firebase.initializeApp(firebaseConfig);
