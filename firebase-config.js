694dce741699b2e32ab8e16c94e98542956cfa59
efao-fc <reinaldomavinga@gmail.com>
2025-07-02 01:32:04 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,3 @@
-// Firebase config já atualizado com dados do projeto real de Reinaldo
-
 const firebaseConfig = {
   apiKey: "AIzaSyD2RoJqL7RtYHNy8_s7Vs2WcpZHC5TNmSE",
   authDomain: "efao-fc-f4f36.firebaseapp.com",
@@ -9,7 +7,4 @@ const firebaseConfig = {
   appId: "1:275309668658:web:d62f98f8cb9c1168d34f2b"
 };
 
-// Inicialização
 firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
