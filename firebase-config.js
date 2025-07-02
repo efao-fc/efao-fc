7208495e164b987640f25ac5ddcf6d5929be1c5c
efao-fc <reinaldomavinga@gmail.com>
2025-07-02 01:17:47 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,15 @@
-// Substitua com suas credenciais do Firebase
+// Firebase config já atualizado com dados do projeto real de Reinaldo
+
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "seu-projeto.firebaseapp.com",
-  projectId: "seu-projeto",
-  storageBucket: "seu-projeto.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef"
+  apiKey: "AIzaSyD2RoJqL7RtYHNy8_s7Vs2WcpZHC5TNmSE",
+  authDomain: "efao-fc-f4f36.firebaseapp.com",
+  projectId: "efao-fc-f4f36",
+  storageBucket: "efao-fc-f4f36.firebasestorage.app",
+  messagingSenderId: "275309668658",
+  appId: "1:275309668658:web:d62f98f8cb9c1168d34f2b"
 };
 
+// Inicialização
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
